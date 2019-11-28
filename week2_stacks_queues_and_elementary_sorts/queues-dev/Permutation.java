d0b4a7997c1f4045dae1522e9a7929e8198505fa
陈万豪 <winnochan@icloud.com>
2019-11-28 23:38:46 +0800

    implement Permutation

--- week2_stacks_queues_and_elementary_sorts/queues-dev/Permutation.java
+++ week2_stacks_queues_and_elementary_sorts/queues-dev/Permutation.java
@@ -1,4 +1,21 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
+        if (args.length <= 0) return;
+
+        // StdOut.println(StdIn.readString());
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> raque = new RandomizedQueue<String>();
+        for (int i = 0; i < k; i++) {
+            String e = StdIn.readString();
+            // StdOut.println(e);
+            raque.enqueue(e);
+        }
+
+        for (String e : raque) {
+            StdOut.println(e);
+        }
     }
 }
