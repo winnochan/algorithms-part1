aa3a002a0f6e38d391d4b2eca8fb1257da0c9dea
陈万豪 <winnochan@icloud.com>
2019-12-02 01:17:36 +0800

    implement Point class

--- week3_mergesort_and_quicksort/collinear-dev/Point.java
+++ week3_mergesort_and_quicksort/collinear-dev/Point.java
@@ -2,14 +2,15 @@
  *  Compilation:  javac Point.java
  *  Execution:    java Point
  *  Dependencies: none
- *  
+ *
  *  An immutable data type for points in the plane.
  *  For use on Coursera, Algorithms Part I programming assignment.
  *
  ******************************************************************************/
 
-import java.util.Comparator;
 import edu.princeton.cs.algs4.StdDraw;
+
+import java.util.Comparator;
 
 public class Point implements Comparable<Point> {
 
@@ -19,8 +20,8 @@ public class Point implements Comparable<Point> {
     /**
      * Initializes a new point.
      *
-     * @param  x the <em>x</em>-coordinate of the point
-     * @param  y the <em>y</em>-coordinate of the point
+     * @param x the <em>x</em>-coordinate of the point
+     * @param y the <em>y</em>-coordinate of the point
      */
     public Point(int x, int y) {
         /* DO NOT MODIFY */
@@ -55,11 +56,23 @@ public class Point implements Comparable<Point> {
      * Double.POSITIVE_INFINITY if the line segment is vertical;
      * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the slope between this point and the specified point
      */
     public double slopeTo(Point that) {
         /* YOUR CODE HERE */
+        if (this.x == that.x && this.y == that.y) {
+            return Double.NEGATIVE_INFINITY;
+        }
+        else if (this.x == that.x) {
+            return Double.POSITIVE_INFINITY;
+        }
+        else if (this.y == that.y) {
+            return .0;
+        }
+        else {
+            return (double) (that.y - this.y) / (that.x - this.x);
+        }
     }
 
     /**
@@ -67,15 +80,24 @@ public class Point implements Comparable<Point> {
      * Formally, the invoking point (x0, y0) is less than the argument point
      * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the value <tt>0</tt> if this point is equal to the argument
-     *         point (x0 = x1 and y0 = y1);
-     *         a negative integer if this point is less than the argument
-     *         point; and a positive integer if this point is greater than the
-     *         argument point
+     * point (x0 = x1 and y0 = y1);
+     * a negative integer if this point is less than the argument
+     * point; and a positive integer if this point is greater than the
+     * argument point
      */
     public int compareTo(Point that) {
         /* YOUR CODE HERE */
+        if (this.y < that.y) {
+            return -1;
+        }
+        else if (this.y == that.y) {
+            return this.x - that.x;
+        }
+        else {
+            return 1;
+        }
     }
 
     /**
@@ -86,6 +108,16 @@ public class Point implements Comparable<Point> {
      */
     public Comparator<Point> slopeOrder() {
         /* YOUR CODE HERE */
+        Point localThis = this;
+        return new Comparator<Point>() {
+            public int compare(Point p1, Point p2) {
+                double slope1 = localThis.slopeTo(p1);
+                double slope2 = localThis.slopeTo(p2);
+                if (slope1 < slope2) return -1;
+                if (slope1 == slope2) return 0;
+                return 1;
+            }
+        };
     }
 
 
@@ -106,5 +138,25 @@ public class Point implements Comparable<Point> {
      */
     public static void main(String[] args) {
         /* YOUR CODE HERE */
+        // Point[] points = new Point[10];
+        //
+        // for (int i = 0; i < points.length; i++) {
+        //     points[i] = new Point(StdRandom.uniform(100), StdRandom.uniform(100));
+        // }
+        //
+        // StdOut.println("========= before sort ==========");
+        // for (int i = 0; i < points.length; i++) {
+        //     StdOut.print(points[i]);
+        //     StdOut.println(points[0].slopeTo(points[i]));
+        // }
+        //
+        // Arrays.sort(points);
+        //
+        // StdOut.println("========= after sort ==========");
+        //
+        // for (int i = 0; i < points.length; i++) {
+        //     StdOut.print(points[i]);
+        //     StdOut.println(points[0].slopeTo(points[i]));
+        // }
     }
 }
